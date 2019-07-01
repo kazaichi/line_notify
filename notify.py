674bbdfa7bc8c3886394416995713d94b2540abe
kazaichi <kazaichi777@gmail.com>
2019-07-01 17:03:50 +0900

    edit notify.py

--- notify.py
+++ notify.py
@@ -1,11 +1,11 @@
-import requests
+from requests import post
 
 def lineNotify(message):
     line_notify_token = 'LINEで取得したトークン'
     line_notify_api = 'https://notify-api.line.me/api/notify'
     payload = {'message': message}
     headers = {'Authorization': 'Bearer ' + line_notify_token} 
-    requests.post(line_notify_api, data=payload, headers=headers)
+    post(line_notify_api, data=payload, headers=headers)
 
 if __name__ == "__main__":
     message = "テスト"
